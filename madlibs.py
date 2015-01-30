09420f60131b6b0ce12cecd6ec9c35a27094a8ed
Hackbright Student <info@hackbrightacademy.com>
2015-01-30 12:55:48 -0800

    Working and pretty. Still need to complete 'harder still'

--- madlibs.py
+++ madlibs.py
@@ -19,16 +19,17 @@ def say_hello():
 @app.route('/greet')
 def greet_person():
     player = request.args.get("person")
-    return render_template("compliment.html", person=player)
-#     AWESOMENESS = [
-#         'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-#         'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
-
-#     compliment = choice(AWESOMENESS)
+    AWESOMENESS = [
+        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
 
 
+    compliments = "I think you're %s, %s, and %s! Do you want to play a game?" % (choice(AWESOMENESS), choice(AWESOMENESS), choice(AWESOMENESS))
 
-@app.route('/game', methods=['GET', 'POST'])
+    return render_template("compliment.html", person=player, compliments=compliments)
+
+
+@app.route('/game')
 def show_game_form():
     if request.args.get("play_game") == "no":
         return render_template("goodbye.html")
@@ -37,13 +38,22 @@ def show_game_form():
 
 @app.route("/madlib", methods=['GET', 'POST'])
 def show_madlib():
-    p_person = request.args.get("person_answer")
-    p_color = request.args.get("color_answer")
-    p_noun = request.args.get("noun_answer")
-    p_adjective = request.args.get("adj_answer")
-    p_verb = request.args.get("verb_answer")
-    p_pet = request.args.get("pet_answer")
-    p_adverb = request.args.get("adv_answer")
+    if request.method == 'POST':
+        p_person = request.form.get("person_answer")
+        p_color = request.form.get("color_answer")
+        p_noun = request.form.get("noun_answer")
+        p_adjective = request.form.get("adj_answer")
+        p_verb = request.form.get("verb_answer")
+        p_pet = request.form.get("pet_answer")
+        p_adverb = request.form.get("adv_answer")
+    else:
+        p_person = request.args.get("person_answer")
+        p_color = request.args.get("color_answer")
+        p_noun = request.args.get("noun_answer")
+        p_adjective = request.args.get("adj_answer")
+        p_verb = request.args.get("verb_answer")
+        p_pet = request.args.get("pet_answer")
+        p_adverb = request.args.get("adv_answer")
 
     AWESOMENESS = ["madlib.html", "madlib2.html", "madlib3.html", "madlib4.html"]
 
