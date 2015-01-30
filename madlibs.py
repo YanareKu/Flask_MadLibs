79cb4e4dc47d27af50d8337d8d4d665782769faf
Hackbright Student <info@hackbrightacademy.com>
2015-01-29 16:06:49 -0800

    everythings working. Hurray!

--- madlibs.py
+++ madlibs.py
@@ -19,24 +19,30 @@ def say_hello():
 @app.route('/greet')
 def greet_person():
     player = request.args.get("person")
+    return render_template("compliment.html", person=player)
+#     AWESOMENESS = [
+#         'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+#         'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
 
-    AWESOMENESS = [
-        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+#     compliment = choice(AWESOMENESS)
 
-    compliment = choice(AWESOMENESS)
 
-    return render_template("compliment.html", person=player, compliment=compliment)
 
 @app.route('/game')
 def show_game_form():
-    player = request.args.get("person")
     if request.args.get("play_game") == "no":
-        return render_template("goodbye.html", person=player)
+        return render_template("goodbye.html")
     else:
-        return render_template()
+        return render_template("game.html")
 
+@app.route("/madlib")
+def show_madlib():
+    p_person = request.args.get("person_answer")
+    p_color = request.args.get("color_answer")
+    p_noun = request.args.get("noun_answer")
+    p_adjective = request.args.get("adj_answer")
 
+    return render_template("madlib.html", person=p_person, color=p_color, noun=p_noun , adjective=p_adjective)
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
